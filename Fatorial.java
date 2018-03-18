46b2da5c5e5149c67e7aac0cd5a8389b674dd67e
Ramon Santos <santosramon1029@gmail.com>
2018-03-18 19:40:06 -0300

    criação de um novo método a parte para o calculo do fatorial a partir de um valor fornecido no médoto main, tratamento para inputs de valor 0 retornando por padrão o valor 1

--- Fatorial.java
+++ Fatorial.java
@@ -1,9 +1,17 @@
 public class Fatorial{
 	public static void main(String[] args){ 
-		int count = 5;
-		for(int i = 4 ; i > 0 ; i--){
-			 count *= i;		
+		long result = calcularFatorial(18);
+		System.out.println(result);
+	}
+
+	public static long calcularFatorial(long num){
+		if(num == 0)
+		return 1;
+
+		for(long i = (num -1) ; i > 0 ; i--){
+			num *= i;
 		}
-		System.out.println(count);
+
+		return num;
 	}
 }
